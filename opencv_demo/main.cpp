40f5513492087d68e26a2a182722f9b01a2b97fe
lh9171338 <2909171338@qq.com>
2021-09-17 12:35:30 +0800

    mingw-4.5.3

---
+++ opencv_demo/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("../lena.jpg");
+	if (img.empty()) {
+		cout << "读取图像失败" << endl;
+		return 0;
+	}
+	namedWindow("游戏原画", 0);
+	imshow("游戏原画", img);
+	waitKey(0);
+	
+	return 0;
+}
